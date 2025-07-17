c10caa846db90a1a864c12b8095e4c9c303cc88d
automatic-updater <automatic-updater@users.noreply.github.com>
2025-07-17 00:42:00 +0000

    update to v3.3.1

--- packages/ddn.nix
+++ packages/ddn.nix
@@ -5,7 +5,7 @@
 , binary-url-pattern
 }:
 let
-  version = "v3.2.0";
+  version = "v3.3.1";
   src-url = version: system: builtins.replaceStrings
     [ "VERSION" "PLATFORM-ARCH" ]
     [ version (go-system system) ]
@@ -17,9 +17,9 @@ let
     "aarch64-darwin" = "darwin-arm64";
   }.${system};
   hash = system: {
-    "linux-amd64" = "sha256-jx43K8BUpNttkwAHWTFMrt0nt9g1wvC8CxzACJxJz8o=";
-    "darwin-amd64" = "sha256-agEnsyPepdGcx+aIxm5NBUzhuggq02tM2w8jQz/3aJQ=";
-    "darwin-arm64" = "sha256-UCrzhFuRX2fD78dePupSpWwpvUZB/c6vpGFvPW4CZAM=";
+    "linux-amd64" = "sha256-Y8qO9/9JYpe7amS0XpvzEG79zBkAleEsOsW02FP46gQ=";
+    "darwin-amd64" = "sha256-lZqp5M1xtKsvw8v8sdbpQ0EyCVIYOq3EQNpAOMbmpDI=";
+    "darwin-arm64" = "sha256-mlhJ+q2qskiTrJDllXCN1kFeAAmzqektpr+WeIg0fX0=";
   }.${system};
   src = system: fetchurl {
     url = src-url version system;
